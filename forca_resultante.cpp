b14fccd5998d275b35e8bec002a228f592cf036b
André Valasco <andrevalasco@hotmail.com>
2021-09-22 19:23:27 -0300

    Update forca_resultante.cpp

--- forca_resultante.cpp
+++ forca_resultante.cpp
@@ -2,8 +2,8 @@
 
 int main () {
 	
-	int fr = 220/20;
-	std::cout << "A forca resultante e: " << fr << " m/s2";
+	int a = 220/20;
+	std::cout << "A forca resultante e: " << a << " m/s2";
 	
 	return 0;
 }
